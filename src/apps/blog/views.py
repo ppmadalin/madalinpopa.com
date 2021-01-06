5886864d9fa1f10baa10adfe08796f28ef8b8a3b
Madalin Popa <contact@madalinpopa.com>
2021-01-06 19:01:02 +0200

    add comment system

--- src/apps/blog/views.py
+++ src/apps/blog/views.py
@@ -1,14 +1,25 @@
-from django.db.models.fields import SlugField
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import get_object_or_404, render, get_list_or_404
 from .models import Post
+from .forms import CommentForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
     """
     List of posts.
     """
-    posts = Post.published.all()
-    return render(request, "blog/index.html")
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, go to first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If the page is out of range, go to last pae of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, "blog/index.html", {'page': page, "posts": posts, })
 
 
 def about(request):
@@ -29,7 +40,7 @@ def post_detail(request, year, month, day, post):
     """
     Post detail.
     """
-    post = get_list_or_404(
+    post = get_object_or_404(
         Post,
         slug=post,
         status="published",
@@ -37,4 +48,15 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
-    return render(request, "blog/posts/detail.html", {"post": post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, "blog/posts/detail.html",
+                  {"post": post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
