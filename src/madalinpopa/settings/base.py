70b639db5662b9941c4b68d66c30884b27621d54
Madalin Popa <contact@madalinpopa.com>
2021-01-07 19:45:05 +0200

    update production.py

--- src/madalinpopa/settings/base.py
+++ src/madalinpopa/settings/base.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = "madalinpopa.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": os.environ.get("DB_HOST"),
+        "NAME": os.environ.get("DB_NAME_BLOG_MADALIN"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
     }
 }
 
--- src/madalinpopa/settings/development.py
+++ src/madalinpopa/settings/development.py
@@ -1,3 +1,8 @@
 from .base import *
 
 ALLOWED_HOSTS = ["*"]
+
+DATABASES['default'] = {
+    "ENGINE": "django.db.backends.sqlite3",
+    "NAME": "db.sqlite3"
+}
