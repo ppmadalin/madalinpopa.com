4a64ee68139cac76bf8031c15ef06eafe7cf916a
Madalin Popa <contact@madalinpopa.com>
2021-01-07 17:41:42 +0200

    update settings

--- src/madalinpopa/settings/production.py
+++ src/madalinpopa/settings/production.py
@@ -9,9 +9,10 @@ ALLOWED_HOSTS = ["https://www.madalinpopa.com", "www.madalinpopa.com", "http://w
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DB_NAME"),
+        "NAME": os.environ.get("DB_NAME_BLOG_MADALIN"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
