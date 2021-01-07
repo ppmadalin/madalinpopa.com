7a59a667af3d34a4e34924181440b28d98e9e4cc
Madalin Popa <contact@madalinpopa.com>
2021-01-07 19:48:25 +0200

    update production.py

--- src/madalinpopa/settings/production.py
+++ src/madalinpopa/settings/production.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = []
 
 ALLOWED_HOSTS = ["https://www.madalinpopa.com", "www.madalinpopa.com", "http://www.madalinpopa.com"]
 
-DATABASES['default'] = {
+DATABASES['production'] = {
     "ENGINE": "django.db.backends.mysql",
     "HOST": os.environ.get("DB_HOST"),
     "NAME": os.environ.get("DB_NAME_BLOG_MADALIN"),
